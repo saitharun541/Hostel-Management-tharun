8e96a7062c8503c54ff8ca6de88d8f6b173020a9
saitharun <saitharun@DESKTOP-TAM6PK4>
2019-07-29 19:48:00 +0530

    done hostel managemant

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -6,22 +6,22 @@ import java.io.*;
 import java.util.*;
 
 public class Hostel{
-	   public static void writedata()throws IOException {
-		   BufferedWriter out = null;
+  public static void writedata()throws IOException {
+  BufferedWriter out = null;
            try{
             out = new BufferedWriter(new FileWriter("allotedhostel.txt"));
             try{
-	       		BufferedReader br = new BufferedReader(new FileReader("hostel.txt"));
-	                   
-	            String line = null;
-	           while ((line = br.readLine()) != null) {
-	               String[] splited = line.split("\\s+");
-	               	 out.write(splited[0]);
-	                 out.newLine();
-	               }
-	           }catch(Exception e){
-	               System.out.println(e);
-	           }
+      BufferedReader br = new BufferedReader(new FileReader("hostel.txt"));
+                  
+           String line = null;
+          while ((line = br.readLine()) != null) {
+              String[] splited = line.split("\\s+");
+              out.write(splited[0]);
+                out.newLine();
+              }
+          }catch(Exception e){
+              System.out.println(e);
+          }
            }catch(Exception e){
                //print
                System.out.println(e);
@@ -29,29 +29,41 @@ public class Hostel{
            }finally{
                out.close();
            }
-	   }
-	   public static boolean readData(String name) {
-		   try{
-	             BufferedReader br = new BufferedReader(new FileReader("hostel.txt"));
-	               String line = null;
-	            
-	               //write your code here !!!
-	               while ((line = br.readLine()) != null) {
-		               String[] splited = line.split("\\s+");
-		               String checkName = splited[0];
-		               //write your code here !!!
-//		               compare check name with name and return true if present and false if not
-	               }
-	               
-	               
-	            }catch(Exception e){
-	                System.out.println(e);
-	            }
-			return true;
-	   }
+  }
+  public static boolean readData(String name) {
+  try{
+            BufferedReader br = new BufferedReader(new FileReader("hostel.txt"));
+              String line = null;
+           
+              //write your code here !!!
+              while ((line = br.readLine()) != null) {
+              String[] splited = line.split("\\s+");
+              String checkName = splited[0];
+              //write your code here !!!
+              
+              if(splited[0].equals(name))
+              {
+           	  return true;
+              }
+//	              compare check name with name and return true if present and false if not
+              }
+              
+              
+           }catch(Exception e){
+               System.out.println(e);
+           }
+return false;
+  }
        public static void allotHostel(){
-    	   //write your code here!!!
-    	   
+      //write your code here!!!
+      
+      try {
+      writedata();
+      }catch(IOException e)
+      {
+      System.out.println(e);
+      }
+      
        }
 
        public static boolean verifyStudent(int regNo){
@@ -73,15 +85,16 @@ public class Hostel{
         }
            
        public static boolean verifyName(String name){
-    	   boolean chk = true;
-    	   
-    	   //write your code here
-    	   
-    	   return chk;
+      boolean chk = true;
+      
+      //write your code here
+      chk = readData(name);
+      
+      return chk;
         }
         
 
-		static String typeName(){
+static String typeName(){
             Scanner sc = new Scanner(System.in);
             String name;
             System.out.println("Enter the student name:(Type exit to exit) ");
@@ -130,4 +143,6 @@ public class Hostel{
         }
        allotHostel();
     }
+
 }
+
